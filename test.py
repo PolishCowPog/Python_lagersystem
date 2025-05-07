a25a9e3978498b8172013b5a32769dcf43d7c4fa
PolishCowPog <156892491+PolishCowPog@users.noreply.github.com>
2025-05-07 09:58:41 +0200

    started on making a menu

--- test.py
+++ test.py
@@ -2,29 +2,93 @@ import json
 import os
 
 
-product_name = "test"
-product_price = 100.0
+# IMAGES ------------------------------------------------------------------------------------------------
 
-file_empty = True
+def print_logo():
+    logo = """
+
+                    __          _                               ____  ____       __                   
+  _____ _____ _____| _|     ___| |_ ___  _ __ ___              |  _ \| __ )     |_ |_____ _____ _____ 
+ |_____|_____|_____| |     / __| __/ _ \| '__/ _ \    _____    | | | |  _ \      | |_____|_____|_____|
+ |_____|_____|_____| |     \__ \ || (_) | | |  __/   |_____|   | |_| | |_) |     | |_____|_____|_____|
+                   | |     |___/\__\___/|_|  \___|             |____/|____/      | |                  
+                   |__|                                                         |__|                  
+
+    """
+    print(logo)
+
+def print_add_product():
+    logo = """
+
+                    __         _       _     _      ____                _            _        __                   
+  _____ _____ _____| _|       / \   __| | __| |    |  _ \ _ __ ___   __| |_   _  ___| |_     |_ |_____ _____ _____ 
+ |_____|_____|_____| |       / _ \ / _` |/ _` |    | |_) | '__/ _ \ / _` | | | |/ __| __|     | |_____|_____|_____|
+ |_____|_____|_____| |      / ___ \ (_| | (_| |    |  __/| | | (_) | (_| | |_| | (__| |_      | |_____|_____|_____|
+                   | |     /_/   \_\__,_|\__,_|    |_|   |_|  \___/ \__,_|\__,_|\___|\__|     | |                  
+                   |__|                                                                      |__|                  
+
+    """
+    print(logo)
 
 
-data = {"Product": product_name, 
-        "Price": product_price}
+# ADD PRODUCT ------------------------------------------------------------------------------------------------
+product_name = "null"
+product_price = 0.0
+product_category = "null"
+product_amount = 0
 
-if not os.path.exists('test.json') or os.path.getsize('test.json') == 0:
-    print("File is empty!")
+
+def add_product_menu():
+    print(f"Product Name: {product_name}")
+    print(f"Product Price: {product_price}")
+    print(f"Product Category: {product_category}")
+    print(f"Product Amount: {product_amount}")
+    print("")
+    print("Chanege values(1-4):")
+    print("1. Product Name")
+    print("2. Product Price")
+    print("3. Product Category")
+    print("4. Product Amount")
+    print("")
+    print("5. Confirm and add product")
+    print("6. Cancel and return to main menu")
+    print("")
+    print("")
+
+
+
+def add_product():
+
+    os.system('cls')
+
+    print_add_product()
+
+    add_product_menu()
+
+    choice = input("Enter your choice (1-4 / 5-6): ")
+    
+
+
     file_empty = True
-else:
-    print("File contains data!")
-    file_empty = False
 
-def remove():
+
+    data = {"Product": product_name, 
+            "Price": product_price,
+            "Category": product_category,
+            "Amount": product_amount}
+
+    if not os.path.exists('test.json') or os.path.getsize('test.json') == 0:
+        print("File is empty!")
+        file_empty = True
+    else:
+        print("File contains data!")
+        file_empty = False
+
     with open('test.json', 'rb+') as file:
         if file_empty == False:
             file.seek(-2, os.SEEK_END)
             file.truncate()
 
-def write():
     with open('test.json', 'at') as file:
         if file_empty == False:
             file.write(",\n")
@@ -36,6 +100,8 @@ def write():
 #remove()
 #write()
 
+# FIND PRODUCT ------------------------------------------------------------------------------------------------
+
 def read_product(product_name):
     with open('test.json', 'r') as file:
         data = json.load(file)
@@ -45,8 +111,29 @@ def read_product(product_name):
     return None
 
 # Example usage:
-product = read_product("test1")
+product = read_product("test2")
 if product:
     print(f"Found product: {product}")
 else:
     print("Product not found")
+
+
+
+# MAIN MENU ------------------------------------------------------------------------------------------------
+def main_menu():
+
+    print_logo()
+
+    choice = input("Enter your choice (1-5): ")
+
+    if choice == "1":
+        add_product()
+    else:
+        os.system('cls')
+        main_menu()
+
+
+
+# RUN PROGRAM --------------------------------------------------------------------------------------------
+os.system('cls')
+main_menu()
